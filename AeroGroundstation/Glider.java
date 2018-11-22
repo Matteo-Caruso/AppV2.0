a085eda6ebf9cf5071d82fbdb267059642e33d43
Srivatsan Srinivasan <srivat97@outlook.com>
2018-11-22 16:36:18 -0500

    -first iterations of container files, created class structures and added member variables

---
+++ AeroGroundstation/Glider.java
@@ -0,0 +1,14 @@
+public class Glider extends Aircraft {
+
+        private double yaw = 0.0;
+        private double  pitch = 0.0;
+        private double roll = 0.0;
+        private double height = 0.0;
+        private double speed = 0.0;
+        private double atitude = 0.0;
+        private double latitude = 0.0;
+        private double longitude = 0.0;
+        private double pid_output_pitch = 0.0;
+        private double pid_outout_yaw = 0.0;
+
+}
