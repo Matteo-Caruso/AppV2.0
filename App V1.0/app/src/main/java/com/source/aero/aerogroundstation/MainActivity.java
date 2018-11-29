17442ebe78561a7161cb57e3794a7bb29d2e50fe
Carl Baron <cbaron3@uwo.ca>
2018-11-29 17:40:22 +0000

    Update App V1.0/app/src/main/java/com/source/aero/aerogroundstation/MainActivity.java, App V1.0/app/src/main/java/com/source/aero/aerogroundstation/MapboxLocationHandler.java files

--- App V1.0/app/src/main/java/com/source/aero/aerogroundstation/MainActivity.java
+++ App V1.0/app/src/main/java/com/source/aero/aerogroundstation/MainActivity.java
@@ -21,16 +21,14 @@ import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 
+import com.source.aero.aerogroundstation.MapboxLocationHandler;
+
 public class MainActivity extends AppCompatActivity implements PermissionsListener, OnMapReadyCallback{
     // Mapbox storage containers
     private MapView mapView;
     private MapboxMap mapboxMap;
 
-    // Permission handling object
-    private PermissionsManager permissionsManager;
-
-    // Storing last location of this device, received from location engine
-    private Location lastKnownLocation;
+    MapboxLocationHandler locationHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +43,8 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
     @Override
     public void onMapReady(MapboxMap mapboxMap) {
         this.mapboxMap = mapboxMap;
-        cameraToCurrentLocation();
+        locationHandler = new LocationHandler(mapboxMap);
+        locationHandler.cameraToCurrentLocation();
     }
 
     @Override
@@ -90,85 +89,12 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
         mapView.onSaveInstanceState(outState);
     }
 
-    // Gets current LatLng from location engine and updates last known location. Sets camera to track location.
-    @SuppressWarnings( {"MissingPermission"})
-    private void cameraToCurrentLocation() {
-        // Check if permissions are enabled and if not request
-        if (PermissionsManager.areLocationPermissionsGranted(this)) {
-            // Activate the MapboxMap LocationComponent to show user location
-            // Adding in LocationComponentOptions is also an optional parameter
-            LocationComponent locationComponent = mapboxMap.getLocationComponent();
-            locationComponent.activateLocationComponent(this);
-            locationComponent.setLocationComponentEnabled(true);
-            // Set the component's camera mode
-            locationComponent.setCameraMode(CameraMode.TRACKING);
-            lastKnownLocation = locationComponent.getLastKnownLocation();
-
-        } else {
-            permissionsManager = new PermissionsManager(this);
-            permissionsManager.requestLocationPermissions(this);
-        }
-    }
-
-    // Gets current LatLng from location engine and updates last known location. Does not set camera to track location.
-    @SuppressWarnings( {"MissingPermission"})
-    private void updateCurrentLocation() {
-        // Check if permissions are enabled and if not request
-        if (PermissionsManager.areLocationPermissionsGranted(this)) {
-            // Activate the MapboxMap LocationComponent to show user location
-            // Adding in LocationComponentOptions is also an optional parameter
-            LocationComponent locationComponent = mapboxMap.getLocationComponent();
-            locationComponent.activateLocationComponent(this);
-            locationComponent.setLocationComponentEnabled(true);
-            // Set the component's camera mode
-            lastKnownLocation = locationComponent.getLastKnownLocation();
-
-        } else {
-            permissionsManager = new PermissionsManager(this);
-            permissionsManager.requestLocationPermissions(this);
-        }
-    }
-
-    // Updates last known location LatLng value. Does not set the camera
-    private void cameraToLocation(Location loc) {
-        CameraPosition position = new CameraPosition.Builder()
-                .target(new LatLng(loc.getLatitude(), loc.getLongitude())) // Sets the new camera position
-                .zoom(17) // Sets the zoom
-                .tilt(30)
-                .build(); // Creates a CameraPosition from the builder
-
-        mapboxMap.animateCamera(CameraUpdateFactory
-                .newCameraPosition(position), 7000);
-    }
-
-    // Handling permission requests
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-    // Prompt user with location services
-    @Override
-    public void onExplanationNeeded(List<String> permissionsToExplain) {
-        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
-    }
-
-    // Handle permission result
-    @Override
-    public void onPermissionResult(boolean granted) {
-        // If we have permission, zoom camera to location and store latest LatLng. Else, Toast out the not granted string
-        if (granted) {
-            cameraToCurrentLocation();
-        } else {
-            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
-            finish();
-        }
-    }
 
     // This button is used for testing if the camera will zoom to our current location on button press and then print out the new latitude and longitude
     public void testButtonLocation(View v) {
-        updateCurrentLocation();
-        cameraToLocation(lastKnownLocation);
+        locationHandler.updateCurrentLocation();
+        locationHandler.cameraToLocation(lastKnownLocation);
+        Location lastKnownLocation = locationHandler.getMyLastLocation();
         Toast.makeText(this,Location.convert(lastKnownLocation.getLatitude(), Location.FORMAT_DEGREES) + " " + Location.convert(lastKnownLocation.getLongitude(), Location.FORMAT_DEGREES), Toast.LENGTH_LONG).show();
     }
 }
---
+++ App V1.0/app/src/main/java/com/source/aero/aerogroundstation/MapboxLocationHandler.java
@@ -0,0 +1,119 @@
+package com.source.aero.aerogroundstation;
+
+import java.util.List;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.location.Location;
+import android.view.View;
+import android.widget.Toast;
+import android.support.annotation.NonNull;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+
+public class MapboxLocationHandler implements PermissionsListener{
+    // Permission handling object
+    private PermissionsManager permissionsManager;
+    private MapboxMap mapboxMap;
+
+    // Storing last location of this device, received from location engine
+    private Location lastKnownLocation;
+
+    LocationHandler(MapboxMap mapboxMap){
+        this.mapboxMap = mapboxMap;
+    }
+
+    public Location getMyLastLocation(){
+        return lastKnownLocation;
+    }
+
+    // Gets current LatLng from location engine and updates last known location. Sets camera to track location.
+    @SuppressWarnings( {"MissingPermission"})
+    public void cameraToCurrentLocation() {
+        // Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            // Activate the MapboxMap LocationComponent to show user location
+            // Adding in LocationComponentOptions is also an optional parameter
+            LocationComponent locationComponent = mapboxMap.getLocationComponent();
+            locationComponent.activateLocationComponent(this);
+            locationComponent.setLocationComponentEnabled(true);
+            // Set the component's camera mode
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+            lastKnownLocation = locationComponent.getLastKnownLocation();
+
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    // Gets current LatLng from location engine and updates last known location. Does not set camera to track location.
+    @SuppressWarnings( {"MissingPermission"})
+    public void updateCurrentLocation() {
+        // Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            // Activate the MapboxMap LocationComponent to show user location
+            // Adding in LocationComponentOptions is also an optional parameter
+            LocationComponent locationComponent = mapboxMap.getLocationComponent();
+            locationComponent.activateLocationComponent(this);
+            locationComponent.setLocationComponentEnabled(true);
+            // Set the component's camera mode
+            lastKnownLocation = locationComponent.getLastKnownLocation();
+
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    // Updates last known location LatLng value. Does not set the camera
+    public void cameraToLocation(Location loc) {
+        CameraPosition position = new CameraPosition.Builder()
+                .target(new LatLng(loc.getLatitude(), loc.getLongitude())) // Sets the new camera position
+                .zoom(17) // Sets the zoom
+                .tilt(30)
+                .build(); // Creates a CameraPosition from the builder
+
+        mapboxMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(position), 7000);
+    }
+
+    // Handling permission requests
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    // Prompt user with location services
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
+    }
+
+    // Handle permission result
+    @Override
+    public void onPermissionResult(boolean granted) {
+        // If we have permission, zoom camera to location and store latest LatLng. Else, Toast out the not granted string
+        if (granted) {
+            cameraToCurrentLocation();
+        } else {
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+
+
+}
+
+
