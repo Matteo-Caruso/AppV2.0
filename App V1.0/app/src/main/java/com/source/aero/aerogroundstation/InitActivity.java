688768911467b6ed81eb66d7507c77fd322fdc9c
Andrew Ning <aning97@gmail.com>
2019-01-14 14:35:05 -0500

    Created initial configuration class and basic UI design for initial configuration activity

---
+++ App V1.0/app/src/main/java/com/source/aero/aerogroundstation/InitActivity.java
@@ -0,0 +1,48 @@
+package com.source.aero.aerogroundstation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class InitActivity extends AppCompatActivity {
+
+    //Required methods
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+}
