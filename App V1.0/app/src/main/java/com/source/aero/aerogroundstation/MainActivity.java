37721871a5cf76c552085a0c54ed6ce20009a55e
Andrew Ning <aning97@gmail.com>
2019-01-31 16:35:53 -0500

    Added on click listeners for main activity speed dial options and main activity bottom navigation bar

--- App V1.0/app/src/main/java/com/source/aero/aerogroundstation/MainActivity.java
+++ App V1.0/app/src/main/java/com/source/aero/aerogroundstation/MainActivity.java
@@ -1,8 +1,11 @@
 package com.source.aero.aerogroundstation;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.leinardi.android.speeddial.SpeedDialActionItem;
 import com.leinardi.android.speeddial.SpeedDialView;
@@ -15,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
     //Ui Elements
     BottomNavigationView bottomNavigationView;
     SpeedDialView speedDialView;
-    Boolean set = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,39 +28,8 @@ public class MainActivity extends AppCompatActivity {
         mapView.onCreate(savedInstanceState);
 
         //Initializing UI Elements
-        bottomNavigationView = (BottomNavigationView) findViewById(R.id.mainActivityBottomNavigationView);
-        speedDialView = findViewById(R.id.mainActivitySpeedDial);
-        speedDialView.addActionItem(
-                new SpeedDialActionItem.Builder(R.id.mainActivitySpeedDialAction1, R.drawable.mapbox_compass_icon)
-                        .setLabel("Option 1")
-                        .create()
-        );
-        speedDialView.addActionItem(
-                new SpeedDialActionItem.Builder(R.id.mainActivitySpeedDialAction2, R.drawable.mapbox_info_icon_default)
-                        .setLabel("Option 2")
-                        .create()
-        );
-        speedDialView.addActionItem(
-                new SpeedDialActionItem.Builder(R.id.mainActivitySpeedDialAction3, R.drawable.ic_search_black_24dp)
-                        .setLabel("Option 3")
-                        .create()
-        );
-        speedDialView.addActionItem(
-                new SpeedDialActionItem.Builder(R.id.mainActivitySpeedDialAction4, R.drawable.mapbox_marker_icon_default)
-                        .setLabel("Option 4")
-                        .create()
-        );
-        /*speedDialView.setOnActionSelectedListener(new SpeedDialView.OnActionSelectedListener() {
-            @Override
-            public boolean onActionSelected(SpeedDialActionItem speedDialActionItem) {
-                switch (speedDialActionItem.getId()) {
-                    case R.id.mainActivitySpeedDialAction1:
-                        set = false;
-                    default:
-                        return false;
-                }
-            }
-        });*/
+        initBottomNavigationBar();
+        initSpeedDial();
 
     }
 
@@ -102,5 +73,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
+    }
+
+    //Performs speed dial initialization
+    private void initSpeedDial() {
+        speedDialView = findViewById(R.id.mainActivitySpeedDial);
+        speedDialView.addActionItem(
+                new SpeedDialActionItem.Builder(R.id.mainActivitySpeedDialAction1, R.drawable.mapbox_compass_icon)
+                        .setLabel("Option 1")
+                        .create()
+        );
+        speedDialView.addActionItem(
+                new SpeedDialActionItem.Builder(R.id.mainActivitySpeedDialAction2, R.drawable.mapbox_info_icon_default)
+                        .setLabel("Option 2")
+                        .create()
+        );
+        speedDialView.addActionItem(
+                new SpeedDialActionItem.Builder(R.id.mainActivitySpeedDialAction3, R.drawable.ic_search_black_24dp)
+                        .setLabel("Option 3")
+                        .create()
+        );
+        speedDialView.addActionItem(
+                new SpeedDialActionItem.Builder(R.id.mainActivitySpeedDialAction4, R.drawable.mapbox_marker_icon_default)
+                        .setLabel("Option 4")
+                        .create()
+        );
+        //On click listener for speed dial options
+        speedDialView.setOnActionSelectedListener(new SpeedDialView.OnActionSelectedListener() {
+            @Override
+            public boolean onActionSelected(SpeedDialActionItem speedDialActionItem) {
+                switch (speedDialActionItem.getId()) {
+                    case R.id.mainActivitySpeedDialAction1:
+                        speedDialView.close();
+                        break;
+                    case R.id.mainActivitySpeedDialAction2:
+                        speedDialView.close();
+                        break;
+                    case R.id.mainActivitySpeedDialAction3:
+                        speedDialView.close();
+                        break;
+                    case R.id.mainActivitySpeedDialAction4:
+                        speedDialView.close();
+                        break;
+                    default:
+                        return true;
+                }
+                return true;
+            }
+        });
+    }
+
+    //Initialize bottom navigation bar
+    private void initBottomNavigationBar() {
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.mainActivityBottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return true;
+            }
+        });
     }
 }
